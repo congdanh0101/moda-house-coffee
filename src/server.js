8feae0df47e737a591c80da9f719a2f01c38cd75
congdanh0101 <congdanh010101>
2022-06-28 00:40:39 +0700

    fix

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-require('dotenv').config()
+// require('dotenv').config()
 const db = require('./config/database')
 const morgan = require('morgan')
 const route = require('./route/index.route')
